b9c8276417830e769c20ec959d39f1c12cea04b3
Dimooon <logu123dima@gmail.com>
2022-02-20 14:22:02 +0200

    Add swagger

--- src/main/java/com/example/task_manager/dto/TaskDto.java
+++ src/main/java/com/example/task_manager/dto/TaskDto.java
@@ -1,5 +1,6 @@
 package com.example.task_manager.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,19 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Сущность task")
 public class TaskDto {
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long Id;
+
+    @Schema(description = "Заголовок задачи")
     private String title;
+
+    @Schema(description = "Состояние task (Выполнена или не выполнена)")
     private boolean isCompleted;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private LocalDate localDate;
 
 }
--- src/main/java/com/example/task_manager/web/TaskWeb.java
+++ src/main/java/com/example/task_manager/web/TaskWeb.java
@@ -3,6 +3,9 @@ package com.example.task_manager.web;
 import com.example.task_manager.dto.TaskDto;
 import com.example.task_manager.entity.Task;
 import com.example.task_manager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(name = "Контроллер task", description = "Взимодействие с task ")
 public class TaskWeb {
     private TaskService taskService;
 
@@ -20,26 +24,34 @@ public class TaskWeb {
 
 
     @PostMapping
+    @Operation(summary = "Создание task",
+            description = "Позволяет создать task пользователя")
     public ResponseEntity<Task> createTask(@RequestBody TaskDto taskDto) {
         Task task = taskService.createTask(taskDto.getTitle(), taskDto.isCompleted());
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @PutMapping
+    @Operation(summary = "Обновление task",
+            description = "Позволяет обновить task пользователя")
     public ResponseEntity<Task> updateTask(@RequestBody TaskDto taskDto) {
         Task task = taskService.updateTask(taskDto.getId(), taskDto.getTitle(), taskDto.isCompleted());
         return ResponseEntity.ok(task);
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary = "получение task",
+            description = "Позволяет получить task пользователя по id")
     public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id) {
         Task task = taskService.gettaskById(id);
         return ResponseEntity.ok(task);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void deleteTaskById(@PathVariable("id") Long id){
-             taskService.deletetaskById(id);
+    @Operation(summary = "Удаление task",
+            description = "Позволяет удалить task пользователя по id")
+    public void deleteTaskById(@PathVariable("id") Long id) {
+        taskService.deletetaskById(id);
     }
 
 }
